d681c267b788931b615f6d7d677ac1ee0b2c07b2
MarijnRongen <MarijnRongen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-18 15:04:39 +0000

    tagging version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/facebook-like-social-widget/tags/1.2@361634 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- facebook_like_social_widget.php
+++ facebook_like_social_widget.php
@@ -3,7 +3,7 @@
 Plugin Name: Facebook Like Social Widget
 Plugin URI: http://www.marijnrongen.com/wordpress-plugins/facebook-like-social-widget/
 Description: Place a Facebook Like button on your Wordpress blog as a widget.
-Version: 1.1
+Version: 1.2
 Author: Marijn Rongen
 Author URI: http://www.marijnrongen.com
 */
@@ -41,9 +41,14 @@ class MR_Like_Widget extends WP_Widget {
 				}
 				break;	
 		}
-		$url = urlencode('http://'.$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]);
+		if (!empty($instance['url'])) {
+			$url = urlencode($instance['url']);
+		}
+		else {
+			$url = urlencode('http://'.$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]);
+		}
 		echo $before_widget;
-		echo "\n	<iframe src=\"http://www.facebook.com/plugins/like.php?href=$url&amp;layout=$layout&amp;show_faces=$show_faces&amp;width=100%&amp;action=like&amp;font&amp;colorscheme=light&amp;height=".$height."px\" scrolling=\"no\" frameborder=\"0\" style=\"border:none; overflow:hidden; width:100%; height:".$height."px;\" allowTransparency=\"true\"></iframe>";
+		echo "\n	<iframe src=\"http://www.facebook.com/plugins/like.php?href=$url&amp;layout=$layout&amp;show_faces=$show_faces&amp;width=100%&amp;action=".$instance['caption']."&amp;font&amp;colorscheme=".$instance['color']."&amp;height=".$height."px\" scrolling=\"no\" frameborder=\"0\" style=\"border:none; overflow:hidden; width:100%; height:".$height."px;\" allowTransparency=\"true\"></iframe>";
 		echo $after_widget;
 	}
 	
@@ -51,11 +56,14 @@ class MR_Like_Widget extends WP_Widget {
 		$instance = $old_instance;
 		$instance['layout'] = $new_instance['layout'];
 		$instance['show_faces'] = isset($new_instance['show_faces']) ? true : false;
+		$instance['caption'] = $new_instance['caption'];
+		$instance['color'] = $new_instance['color'];
+		$instance['url'] = strip_tags($new_instance['url']);
 		return $instance;
 	}
 	
 	function form($instance) {
-		$instance = wp_parse_args((array) $instance, array( 'layout' => 'standard', 'show_faces' => false));
+		$instance = wp_parse_args((array) $instance, array( 'layout' => 'standard', 'show_faces' => false, 'caption' => like, 'color' =>light, 'url' => ''));
 		?>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'layout' ); ?>">Button layout:</label>
@@ -69,6 +77,24 @@ class MR_Like_Widget extends WP_Widget {
 			<input class="checkbox" type="checkbox" <?php checked( $instance['show_faces'], true ); ?> id="<?php echo $this->get_field_id( 'show_faces' ); ?>" name="<?php echo $this->get_field_name( 'show_faces' ); ?>" />
 			<label for="<?php echo $this->get_field_id( 'show_faces' ); ?>">Show faces (only when using standard layout)</label>
 		</p>	
+		<p>
+			<label for="<?php echo $this->get_field_id( 'caption' ); ?>">Caption:</label>
+			<select id="<?php echo $this->get_field_id( 'caption' ); ?>" name="<?php echo $this->get_field_name( 'caption' ); ?>" class="widefat" style="width:100%;">
+				<option <?php if ( "like" == $instance['caption'] ) echo 'selected="selected"'; ?> value="like">Like</option>
+				<option <?php if ( "recommend" == $instance['caption'] ) echo 'selected="selected"'; ?> value="recommend">Recommend</option>
+			</select>
+		</p>	
+		<p>
+			<label for="<?php echo $this->get_field_id( 'color' ); ?>">Color scheme:</label>
+			<select id="<?php echo $this->get_field_id( 'color' ); ?>" name="<?php echo $this->get_field_name( 'color' ); ?>" class="widefat" style="width:100%;">
+				<option <?php if ( "light" == $instance['color'] ) echo 'selected="selected"'; ?> value="light">Light</option>
+				<option <?php if ( "dark" == $instance['color'] ) echo 'selected="selected"'; ?> value="dark">Dark</option>
+			</select>
+		</p>	
+		<p>
+			<label for="<?php echo $this->get_field_id( 'url' ); ?>">URL to like / recommend (<b>Optional</b> leave empty for the URL of the page the button is on):</label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'url' ); ?>" name="<?php echo $this->get_field_name( 'url' ); ?>" value="<?php echo $instance['url']; ?>" />
+		</p>
 		<?php
 	}
 }
